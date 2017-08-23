912b048fe53f77ca5d8c96e5c63db89b49b60ecc
shadow <thelordsnake@hotmail.com>
2017-08-23 15:30:50 -0500

    osteo2

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <?php include 'includes/header.php';?>
+
+    <title>Osteo Sin Max </title>
+</head>
+<body>
+
+<div class="container">
+	<header class="row">
+		<div class="col">
+		<h1 id="texto-centrado">Osteo Sin Max</h1>
+		</div>
+	</header>
+	<section class="contendedor-main row">
+		<div class="columna-1 col-md-4 align-self-center" id="texto-centrado">
+		<h2>OSTEO SIN MAX</h2>
+		<img id= jaina src="img/OSTEO SIN MAX.jpg" alt="jaina">
+		</div>
+		<main class=" col-md-8">
+			<h2 id="texto-centrado">Historia en Breve -
+			DOS EXÁMENES </h2>
+			<p id="texto-justificado">
+			Hacer análisis de detección de forma excesiva e innecesaria a la larga podría causar una cascada de costes de seguimiento obligatorios, junto con tratamientos posiblemente invasivos que en realidad no necesita, así como el trauma psicológico subsecuente.
+			Dos típicas pruebas de detección, que probablemente podrían hacerle más perjuicio que beneficio, son las mamografías anuales, en el caso de las mujeres; y la prueba anual de PSA, en el caso de los hombres. Los estudios han demostrado que estas pruebas no tienen ningún impacto en las tasas de mortalidad, y que una mayor cantidad de personas son perjudicadas por el tratamiento innecesario, debido a estas pruebas, en comparación con las personas que son salvadas, como resultado de un diagnóstico temprano
+			Un reciente estudio de meta-análisis encontró que los controles de salud generales no disminuían la morbilidad ni la mortalidad, ni en términos generales, ni en cuanto a las causas cardiovasculares o de cáncer, aunque si aumentó el número de nuevos diagnósticos
+			Otro estudio advierte contra tener demasiada fe en estudios médicos que demuestran efectos muy extensos causados por tratamientos médicos (ya que beneficien o hagan daño). El análisis masivo que monitoreo el destino de miles de estudios desde los efectos demostrados en el estudio inicial, en comparación con los efectos dilucidados en ensayos posteriores. En el 90 % de los casos en donde se informó inicialmente efectos “muy extensos”, tales efectos se redujeron o desaparecieron por completo conforme se realizaron estudios posteriores para confirmar los resultados</p>
+		</main>
+	</section>
+	<section class="row widgets justify-content-between">
+		<div class="col-12 col-md-4 col-lg-3">
+			<p>HOLA BUEN DÍA, SOLO QUIERO COMENTARLES QUE QUE NO TENEMOS TIENDA ONLINE EN LA CIUDAD DE MÉXICO
+			NUESTRAS OFICINAS DISTRIBUIDORAS SE ENCUENTRAN UBICADAS EN PUEBLA COATZACOALCOS Y MÉRIDA. TAMPOCO VENDEMOS EL PRODUCTO PIRATA
+			NUESTRO PRODUCTO EN ORIGINAL Y TAMBIÉN LA LÍNEA PYR TEX COMO EL OVARIFEN, PULMOCALCIO, CICATRISAN ETC.
+			</p>
+		</div>
+			
+		<div class="col-12 col-md-4 col-lg-3">
+			<p>HOLA BUEN DÍA, SOLO QUIERO COMENTARLES QUE QUE NO TENEMOS TIENDA ONLINE EN LA CIUDAD DE MÉXICO
+			NUESTRAS OFICINAS DISTRIBUIDORAS SE ENCUENTRAN UBICADAS EN PUEBLA COATZACOALCOS Y MÉRIDA. TAMPOCO VENDEMOS EL PRODUCTO PIRATA
+			NUESTRO PRODUCTO EN ORIGINAL Y TAMBIÉN LA LÍNEA PYR TEX COMO EL OVARIFEN, PULMOCALCIO, CICATRISAN ETC.
+			</p>
+		</div>
+			
+		<div class="col-12 col-md-4 col-lg-3">
+		<p>HOLA BUEN DÍA, SOLO QUIERO COMENTARLES QUE QUE NO TENEMOS TIENDA ONLINE EN LA CIUDAD DE MÉXICO
+			NUESTRAS OFICINAS DISTRIBUIDORAS SE ENCUENTRAN UBICADAS EN PUEBLA COATZACOALCOS Y MÉRIDA. TAMPOCO VENDEMOS EL PRODUCTO PIRATA
+			NUESTRO PRODUCTO EN ORIGINAL Y TAMBIÉN LA LÍNEA PYR TEX COMO EL OVARIFEN, PULMOCALCIO, CICATRISAN ETC.
+			</p></div>
+	</section>
+	<footer class="row">
+		<div class="col">
+			<h4>foter</h4>
+		</div>
+	</footer>
+</div>
+
+<?php include 'includes/footer.php';?>
+</body>
+</html>
